63149a67a2cb348a3971de34fe347bc63af64fce
Alphabb07 <162568674+Alphabb07@users.noreply.github.com>
2024-12-18 07:55:47 +0100

    Last update
    Aggiustati tutti gli errori di sintassi e reso il codice piu efficiente in modo tale da ottenere il risultato ottenuto, inoltre è stato trovato un problema nella sezione dell'oggetto getmiliseconds() con il quale era stato scritto male. Il codice funziona correttmente adesso.

--- STATIC/script.js
+++ STATIC/script.js
@@ -1,49 +1,43 @@
 
-let cerchio
-let cerchio2
-let cerchio3
-let punteggio = 0
-let data1
-let data2
-let ms1
-let ms2
+let cerchio;
+let cerchio2;
+let cerchio3;
+let punteggio = 0;
+let data1;
+let data2;
+let ms1;
+let ms2;
 
 function startGame() {
-    cerchio1 = document.querySelector(".cerchio1")
-    cerchio2 = document.querySelector(".cerchio2")
-    cerchio3 = document.querySelector(".cerchio3")
+    cerchio1 = document.querySelector(".cerchio1");
+    cerchio2 = document.querySelector(".cerchio2");
+    cerchio3 = document.querySelector(".cerchio3");
 
-    cerchio1.classList.remove("red")
-    cerchio1.classList.add("green")
+    cerchio1.classList.remove("red");
+    cerchio1.classList.add("green");
 
-    setTimeout(secondaLuce, 1000)
+    setTimeout(secondaLuce, 1000);
 }
 
 function secondaLuce() {
-    cerchio2.classList.remove("red")
-    cerchio2.classList.add("green")
+    cerchio2.classList.remove("red");
+    cerchio2.classList.add("green");
 
-    setTimeout(terzaLuce, 450)
+    setTimeout(terzaLuce, 450);
 }
 
 function terzaLuce() {
-    cerchio3.classList.remove("red")
-    cerchio3.classList.add("green")
+    cerchio3.classList.remove("red");
+    cerchio3.classList.add("green");
 
-    data1 = new Date()
+    data1 = new Date();
 }
 
 function testaRiflessi() {
-    data2 = new Date()
-
-    ms1 = date1.getMilliSeconds()
-    ms2 = date2.getMilliSeconds()
-
-    punteggio = ms2 - ms1
-    document.querySelector()
-    document.querySelector(".testo").innerHTML = punteggio
-
-}
-
-
-console.log(punteggio);
+    data2 = new Date();
+    if(data1){
+        punteggio = data2 - data1;
+        let textpoint = document.querySelector(".testo");
+        textpoint.innerHTML = "Tempo di reazione: " + punteggio + " ms";
+        }
+        }
